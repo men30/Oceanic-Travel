040c3f549d532cfa82207103907b38d5058060a5
men30 <airon2845@gmail.com>
2025-05-14 08:08:24 +0300

    Add files via upload

---
+++ script.js
@@ -0,0 +1,18 @@
+// Мобильное меню
+const burger = document.querySelector(".burger");
+const navLinks = document.querySelector(".nav-links");
+
+burger.addEventListener("click", () => {
+  navLinks.classList.toggle("active");
+  burger.classList.toggle("toggle");
+});
+
+// Плавная прокрутка
+document.querySelectorAll('a[href^="#"]').forEach((anchor) => {
+  anchor.addEventListener("click", function (e) {
+    e.preventDefault();
+    document.querySelector(this.getAttribute("href")).scrollIntoView({
+      behavior: "smooth",
+    });
+  });
+});
